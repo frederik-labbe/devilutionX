f0887f0e1657ce9c47b082e0c3f749eb4835c0f5
Xadhoom <>
2021-04-21 05:59:54 +0000

    Bugfix maximal experience

--- Source/player.cpp
+++ Source/player.cpp
@@ -938,8 +938,6 @@ void NextPlrLevel(int pnum)
 	CalcPlrInv(pnum, true);
 }
 
-#define MAXEXP 2000000000
-
 void AddPlrExperience(int pnum, int lvl, int exp)
 {
 	int powerLvlCap, expCap, newLvl, i;
@@ -980,16 +978,17 @@ void AddPlrExperience(int pnum, int lvl, int exp)
 	}
 
 	plr[pnum]._pExperience += exp;
-	if ((DWORD)plr[pnum]._pExperience > MAXEXP) {
-		plr[pnum]._pExperience = MAXEXP;
+	if ((DWORD)plr[pnum]._pExperience > ExpLvlsTbl[MAXCHARLEVEL - 1]) {
+		plr[pnum]._pExperience = ExpLvlsTbl[MAXCHARLEVEL - 1];
 	}
 
 	if (sgOptions.Gameplay.bExperienceBar) {
 		force_redraw = 255;
 	}
 
-	if (plr[pnum]._pExperience >= ExpLvlsTbl[49]) {
-		plr[pnum]._pLevel = 50;
+	if (plr[pnum]._pExperience >= ExpLvlsTbl[MAXCHARLEVEL - 2]) {
+		plr[pnum]._pLevel = MAXCHARLEVEL - 1;
+		plr[pnum]._pNextExper = ExpLvlsTbl[MAXCHARLEVEL - 1];
 		return;
 	}
 
